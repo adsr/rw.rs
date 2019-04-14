10888a86cd8e35748b66c59d9187bddcfd12dfc9
Adam Saponara <as@php.net>
2019-04-14 18:58:28 -0400

    update bin/bootstrap.sh

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -6,21 +6,21 @@ source "$(cd $(dirname "${BASH_SOURCE[0]}") &>/dev/null && pwd)/common.sh"
 apt install -y build-essential libtool sudo net-tools curl git zsh vim emacs nano mle screen tmux irssi weechat inspircd
 
 # configure sshd
-cp -vf "$rwrs_root/config/sshd_config" /etc/ssh/
+cp -vf "$rwrs_root/etc/sshd_config" /etc/ssh/
 systemctl restart sshd
 
 # configure restricted user slice
 mkdir -p $restricted_slice_dir
-cp -vf "$rwrs_root/config/user-restricted.slice.conf" $restricted_slice_dir
+cp -vf "$rwrs_root/etc/user-restricted.slice.conf" $restricted_slice_dir
 systemctl daemon-reload
 
 # configure cron
-cp -vf "$rwrs_root/config/cron" /etc/cron.d/rw-rs
+cp -vf "$rwrs_root/etc/cron" /etc/cron.d/rw-rs
 
 # configure ircd
 touch /etc/motd
 ln -vfs /etc/motd /etc/inspircd/inspircd.motd
-cp -vf "$rwrs_root/config/inspircd.conf" /etc/inspircd/inspircd.conf
+cp -vf "$rwrs_root/etc/inspircd.conf" /etc/inspircd/inspircd.conf
 ircd_pass=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 24)
 sed -i "s/@ircdpasshere@/$ircd_pass/g" /etc/inspircd/inspircd.conf
 chown -R irc:irc /etc/inspircd/
