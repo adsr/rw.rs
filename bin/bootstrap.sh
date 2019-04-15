dd017fe21a09a956198eee81b6b82fd1c025530d
Adam Saponara <as@php.net>
2019-04-14 21:16:28 -0400

    configure disk quotas

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -3,7 +3,14 @@ set -eu
 source "$(cd $(dirname "${BASH_SOURCE[0]}") &>/dev/null && pwd)/common.sh"
 
 # install packages
-apt install -y build-essential libtool sudo net-tools curl git zsh vim emacs nano mle screen tmux irssi weechat inspircd
+apt install -y build-essential libtool sudo quota net-tools curl git zsh vim emacs nano mle screen tmux irssi weechat inspircd
+
+# configure quota
+awk -vq=$quota_path '{if($2==q){ $4=$4",usrjquota=/aquota.user,jqfmt=vfsv1" } print}' /etc/fstab >/etc/fstab.new
+mv -vf /etc/fstab.new /etc/fstab
+mount -vo remount $quota_path
+quotacheck -ucm $quota_path
+quotaon -v $quota_path
 
 # configure sshd
 cp -vf "$rwrs_root/etc/sshd_config" /etc/ssh/
@@ -29,6 +36,6 @@ chmod 600 /etc/inspircd/inspircd.conf
 systemctl restart inspircd
 
 # TODO tls ircd
-# TODO apache with per-user web dirs
+# TODO httpd with per-user web dirs
 # TODO simple alerting
 # TODO smoker tests
--- bin/common.sh
+++ bin/common.sh
@@ -4,4 +4,7 @@ rwrs_root="$(dirname $(cd $(dirname "${BASH_SOURCE[0]}") &>/dev/null && pwd))"
 rwrs_url='https://github.com/adsr/rw.rs'
 max_uname_len=10
 restricted_slice_dir=/etc/systemd/system/user-restricted.slice.d
+quota_path='/home'
+quota_soft='960M'
+quota_hard='1024M'
 default_user_motd='...'
