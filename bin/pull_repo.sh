17c8afd4eb39ebfa11a659800d17132ba45d1835
Adam Saponara <as@php.net>
2019-04-21 01:06:02 -0400

    do not clean files in gitignore

--- bin/pull_repo.sh
+++ bin/pull_repo.sh
@@ -5,5 +5,5 @@ source "$(cd $(dirname "${BASH_SOURCE[0]}") &>/dev/null && pwd)/common.sh"
 [ -f "$rwrs_root/$do_not_pull_fname" ] && exit
 
 git -C "$rwrs_root" reset --hard
-git -C "$rwrs_root" clean -fdx
+git -C "$rwrs_root" clean -fd
 git -C "$rwrs_root" pull --rebase
