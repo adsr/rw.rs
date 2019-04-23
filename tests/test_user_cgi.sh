5d1ed84332a66e735dec34ffd8e2b0909ac2fc9d
Adam Saponara <as@php.net>
2019-04-23 01:52:37 -0400

    fix per-user cgi
    65c1046 did not actually work

---
+++ tests/test_user_cgi.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+ok_marker=$(date +%s)
+http_res_b64=$(echo -en "HTTP/1.1 200 OK\r\n\r\n$ok_marker" | base64)
+
+cmd=''
+cmd+='mkdir -p public_html; '
+cmd+='timeout 5 socat '
+cmd+="UNIX-LISTEN:/home/$test_user/public_html/cgi-bin.sock,fork,perm-early=0666 "
+cmd+="SYSTEM:'echo $http_res_b64 | base64 -d' &>/dev/null & "
+cmd+="curl -m5 -s localhost/~$test_user/cgi-bin/test; wait"
+
+assert $ok_marker \
+    "$(test_cmd "$cmd")" \
+    'user cgi should work'
