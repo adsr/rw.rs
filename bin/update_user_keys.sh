2422c2fa101672572a4cf9843219e32f0b7bd6eb
Adam Saponara <as@php.net>
2019-12-24 15:41:57 -0500

    ensure ssh dir exists before copying over keys

--- bin/update_user_keys.sh
+++ bin/update_user_keys.sh
@@ -7,10 +7,13 @@ log_ns='update_user_keys.sh'
 ls -1 "$rwrs_root/users" | sort | while read uname; do
     repo_auth_keys="$rwrs_root/users/$uname/authorized_keys"
     home_dir=$(getent passwd $uname | cut -d: -f6)
-    home_auth_keys="$home_dir/.ssh/authorized_keys"
+    ssh_dir="$home_dir/.ssh"
+    home_auth_keys="$ssh_dir/authorized_keys"
     if ! diff $repo_auth_keys $home_auth_keys; then
+        mkdir -p "$ssh_dir"
+        chmod 700 "$ssh_dir"
         cp -vf $repo_auth_keys $home_auth_keys
-        chown -v "$uname:$uname" $home_auth_keys
+        chown -v -R "$uname:$uname" $ssh_dir
         logger -t $log_ns "updated ssh keys for $uname"
     fi
 done
