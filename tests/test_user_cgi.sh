c6ef4dd3f51e7178d3e9e9a823db7c09bb2f4461
Adam Saponara <as@php.net>
2019-12-24 13:19:14 -0500

    Rename `cgi-bin` to `proxy`

--- tests/test_user_cgi.sh
+++ tests/test_user_cgi.sh
@@ -6,10 +6,10 @@ http_res_b64=$(echo -en "HTTP/1.1 200 OK\r\n\r\n$ok_marker" | base64)
 cmd=''
 cmd+='mkdir -p ~/public_html; '
 cmd+='timeout 5 socat '
-cmd+="UNIX-LISTEN:/home/$test_user/public_html/cgi-bin.sock,fork,perm-early=0666 "
+cmd+="UNIX-LISTEN:/home/$test_user/public_html/proxy.sock,fork,perm-early=0666 "
 cmd+="SYSTEM:'echo $http_res_b64 | base64 -d' &>/dev/null & "
-cmd+="sleep 1; curl -m5 -s localhost/~$test_user/cgi-bin/test; wait"
+cmd+="sleep 1; curl -m5 -s localhost/~$test_user/proxy/test; wait"
 
 assert $ok_marker \
     "$(test_cmd "$cmd")" \
-    'user cgi should work'
+    'user proxypass should work'
