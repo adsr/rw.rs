a298661c48f59c8f45b0b8bf5bfba4e97b4a68b1
Adam Saponara <as@php.net>
2020-03-13 22:32:59 -0400

    update bin/gen_footer.sh

--- bin/gen_footer.sh
+++ bin/gen_footer.sh
@@ -5,9 +5,12 @@ source "$(cd $(dirname "${BASH_SOURCE[0]}") &>/dev/null && pwd)/common.sh"
 html=''
 html+='<ul>'$'\n'
 for uname in $(ls -1 "$rwrs_root/users" | sort); do
-    test -d "/home/$uname/public_html" || continue
-    html+="<li><a href='/~$uname'>$uname</a></li>"$'\n'
+    if [ -d "/home/$uname/public_html" ]; then
+        html+="<li><a href='/~$uname'>$uname</a></li>"$'\n'
+    else
+        html+="<li>$uname</li>"$'\n'
+    fi
 done
 html+="</ul>"$'\n'
-html+="<p><a href='https://github.com/adsr/rw.rs/'>and you?</a></p>"
+html+="<p><a href='https://github.com/adsr/rw.rs'>Join</a></p>"
 echo "$html" >"$htdocs_root/FOOTER.html"
