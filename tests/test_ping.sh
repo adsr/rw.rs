d185dfe2cfed8d8b4fb9ab9fdc3dd8e238877936
Adam Saponara <as@php.net>
2020-03-13 20:10:39 -0400

    Allow network to irc.tilde.chat and friends

--- tests/test_ping.sh
+++ tests/test_ping.sh
@@ -3,3 +3,7 @@
 assert yes \
     $(test_cmd 'ping -W5 -c1 github.com &>/dev/null && echo yes || echo no') \
     'should be able to ping GitHub'
+
+assert yes \
+    $(test_cmd 'ping -W5 -c1 irc.tilde.chat &>/dev/null && echo yes || echo no') \
+    'should be able to ping irc.tilde.chat'
