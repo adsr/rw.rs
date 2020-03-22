9fc5a611201439e483ec8bbaa330e0b3ea9610a4
Adam Saponara <as@php.net>
2020-03-21 20:54:17 -0400

    Allow network access to freenode and oftc (#23)

--- tests/test_ping.sh
+++ tests/test_ping.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for hostname in github.com club.tilde.chat raw.githubusercontent.com; do
+for hostname in github.com club.tilde.chat raw.githubusercontent.com chat.freenode.net irc.oftc.net; do
     assert yes \
         $(test_cmd "ping -W5 -c1 $hostname &>/dev/null && echo yes || echo no") \
         "should be able to ping $hostname"
