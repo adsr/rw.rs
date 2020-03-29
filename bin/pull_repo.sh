9f0b3b06fa53cf0d90a9cbc4e7cd6de088ee63f3
Adam Saponara <as@php.net>
2020-03-29 01:05:08 -0400

    update submodules when pulling repo (2)

--- bin/pull_repo.sh
+++ bin/pull_repo.sh
@@ -6,5 +6,5 @@ source "$(cd $(dirname "${BASH_SOURCE[0]}") &>/dev/null && pwd)/common.sh"
 
 git -C "$rwrs_root" reset --hard
 git -C "$rwrs_root" clean -fd
-git -C "$rwrs_root" pull --rebase --recurse-submodules
-git -C "$rwrs_root" submodule update --init
+git -C "$rwrs_root" pull --rebase
+git -C "$rwrs_root" submodule update --recursive --remote
