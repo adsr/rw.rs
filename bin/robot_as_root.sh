b88c1b6e6c6f1951ce707516a4eeec9cb96ac4f7
Adam Saponara <as@php.net>
2020-04-29 21:47:11 -0400

    archive print.rw.rs

--- bin/robot_as_root.sh
+++ bin/robot_as_root.sh
@@ -6,14 +6,8 @@ task=${1:-}
 
 main() {
     case $task in
-        printcap_upload) $task ;;
         *) exit 1 ;;
     esac
-}
-
-printcap_upload() {
-    cat | su -s/bin/bash -c "base64 -d >$printcap_image.bak" apache
-    file -i $printcap_image.bak | grep image/webp && mv -f $printcap_image.bak $printcap_image
 }
 
 main
