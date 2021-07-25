311e40841452efcdb91c74415bd5e5cb0b700ba9
Adam Saponara <as@php.net>
2021-07-24 21:37:30 -0400

    update test_ping.sh

--- tests/test_ping.sh
+++ tests/test_ping.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for hostname in github.com club.tilde.chat raw.githubusercontent.com chat.freenode.net irc.oftc.net; do
+for hostname in github.com club.tilde.chat raw.githubusercontent.com irc.libera.chat irc.oftc.net; do
     assert ok \
         $(test_cmd "{ for i in \$(seq 1 5); do ping -W5 -c1 $hostname &>/dev/null && break; done && echo ok; } || echo fail") \
         "should be able to ping $hostname"
