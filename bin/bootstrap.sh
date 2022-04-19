9eed0ca50510b1be4be329a94c89c5019514fc17
MSPE <mspe@tilde.link>
2022-04-19 13:46:08 +0200

    add rsync package to bootstrap

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -23,7 +23,7 @@ if [ -z "${RWRS_SKIP_APT+x}" ]; then
         libsqlite3-dev fish mosh stow re2c bison libssl-dev pkg-config \
         zlib1g-dev libreadline-dev libgd-dev libfreetype6-dev libwebp-dev \
         libonig-dev lua5.3 liblua5.3-dev libffi-dev bind9-dnsutils cmake \
-        ca-certificates debian-archive-keyring snapd
+        ca-certificates debian-archive-keyring snapd rsync
     systemctl daemon-reexec
     logger -t $log_ns "ran apt updates"
 fi
