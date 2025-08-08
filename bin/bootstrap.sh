d936790175d20383e517cf2300bf4dc7061e5322
Sohil876 <sohil876@gmail.com>
2025-08-08 16:35:13 +0530

    install python3.13-venv

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -23,7 +23,7 @@ if [ -z "${RWRS_SKIP_APT+x}" ]; then
         libsqlite3-dev fish mosh stow re2c bison libssl-dev pkg-config \
         zlib1g-dev libreadline-dev libgd-dev libfreetype6-dev libwebp-dev \
         libonig-dev lua5.4 liblua5.4-dev libffi-dev bind9-dnsutils cmake \
-        ca-certificates debian-archive-keyring snapd rsync unattended-upgrades
+        ca-certificates debian-archive-keyring snapd rsync unattended-upgrades python3.13-venv
     systemctl daemon-reexec
     logger -t $log_ns "ran apt updates"
 fi
